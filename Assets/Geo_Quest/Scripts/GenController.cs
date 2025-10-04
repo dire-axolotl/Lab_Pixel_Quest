c9972da92390be62e9462bc49908686b1c2d3746
dire-axolotl <langley.dean.michael@gmail.com>
2025-10-04 12:20:44 -0400

    a

--- Assets/Geo_Quest/Scripts/GenController.cs
+++ Assets/Geo_Quest/Scripts/GenController.cs
@@ -9,29 +9,36 @@ public class GenController : MonoBehaviour
     float y = 0f;
     float z = 0f;
     // Start is called before the first frame update
+    private Rigidbody2D rb;
+    public float speed = 10f;
     void Start()
     {
-      
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            transform.position += new Vector3(0, 1, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            transform.position += new Vector3(0, -1, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            transform.position += new Vector3(-1, 0, 0);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            transform.position += new Vector3(1, 0, 0);
-        }
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2 (xInput*speed, rb.velocity.y);
+        //float yInput = Input.GetAxis("Vertical");
+        //rb.velocity = new Vector2(xInput, rb.velocity.y);
+        Debug.Log(xInput);
+        //if (Input.GetKeyDown(KeyCode.W))
+        //{
+        //    rb.velocity = new Vector2(rb.velocity.x, 10);
+        //}
+        //if (Input.GetKeyDown(KeyCode.S))
+        //{
+        //    rb.velocity = new Vector2(rb.velocity.x, -10);
+        //}
+        //if (Input.GetKeyDown(KeyCode.A))
+        //{
+        //    rb.velocity = new Vector2(-10, rb.velocity.y);
+        //}
+        //if (Input.GetKeyDown(KeyCode.D))
+        //{
+        //    rb.velocity = new Vector2(10, rb.velocity.y);
+        //}
     }
 }
--- Assets/Geo_Quest/Scripts/RotationTest.cs
+++ Assets/Geo_Quest/Scripts/RotationTest.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,12 @@ using UnityEngine;
 public class rotationTest : MonoBehaviour
 {
     // Start is called before the first frame update
+    Quaternion change = Quaternion.Euler(1000, 1000, 1000);
+    private Rigidbody2D rb;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -29,6 +33,9 @@ public class rotationTest : MonoBehaviour
         {
             transform.position += new Vector3(.02f, 0, 0);
         }
-
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.rotation = Quaternion.Slerp(Quaternion.identity, change, .1f);
+        }
     }
 }
