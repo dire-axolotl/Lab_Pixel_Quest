d1d0e74198848985a0b4c7a26aff4afc50278c96
dire-axolotl <langley.dean.michael@gmail.com>
2025-10-04 11:03:18 -0400

    d
    d

---
+++ Assets/Geo_Quest/Scripts/GenController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenController : MonoBehaviour
+{
+    string var1 = "hello";
+    float x  = .0001f;
+    float y = 0f;
+    float z = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+    }
+}
