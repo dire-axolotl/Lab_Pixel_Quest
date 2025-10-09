2650fb9c3f41e6d03a9ec0d2fe7b154de16586ca
dire-axolotl <langley.dean.michael@gmail.com>
2025-10-08 21:41:48 -0400

    d

--- Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerMovement.cs
@@ -1,6 +1,28 @@
 using UnityEngine;
-
 public class HW2PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D _rb;
+    private float xSpeed;
+    private float ySpeed;
 
+
+    public float speed = 3;
+
+    private string InputX = "Horizontal";
+    private string InputY = "Vertical";
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        xSpeed = Input.GetAxis("Horizontal");
+        ySpeed = Input.GetAxis("Vertical");
+
+        _rb.velocity = new Vector2(xSpeed, ySpeed) * speed;
+        
+
+    }
 }
--- Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
+++ Assets/Hands-On Homework #2/Scripts/HW2PlayerRotation.cs
@@ -2,5 +2,25 @@ using UnityEngine;
 
 public class HW2PlayerRotation : MonoBehaviour
 {
+    private Camera cam;
 
+    private string camName = "Game_Camera";
+
+    private Vector3 mousePos;
+
+    private void Start()
+    {
+        cam = GameObject.Find(camName).GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        
+        Vector3 pos = mousePos - transform.position;
+
+        float rotZ = Mathf.Atan2(pos.y, pos.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, rotZ - 90);
+    }
 }
