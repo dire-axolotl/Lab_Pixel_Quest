6b913d02e0fa629eb8cfc7fbc7487cbd5f90bae4
dire-axolotl <langley.dean.michael@gmail.com>
2025-10-11 14:23:03 -0400

    DD

--- Assets/Geo_Quest/Scripts/GeoController.cs
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEditor.SearchService;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEditor.Search;
 
 public class GenController : MonoBehaviour
 {
     // Start is called before the first frame update
     private Rigidbody2D rb;
+    private SpriteRenderer rbSpriteRender;
+
+    public Color color1;
+    public Color color2;
+    public Color color3;
 
     public float speed = 5f;
     public string nextLevel = "Geo_Quest_Scene_2";
@@ -15,6 +22,7 @@ public class GenController : MonoBehaviour
     {
         rb = GetComponent<Rigidbody2D>();
         //hitColider = GetComponent<Collider>();
+        rbSpriteRender = rb.GetComponent<SpriteRenderer>();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -36,13 +44,29 @@ public class GenController : MonoBehaviour
         }
     }
 
+    private void colorChanger()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            rbSpriteRender.color = color1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            rbSpriteRender.color = color2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            rbSpriteRender.color = color3;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
         float xInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(xInput * speed, rb.velocity.y);
         //Debug.Log(xInput);
-
+        colorChanger();
 
     }
 }
