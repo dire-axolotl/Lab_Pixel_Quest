e12709883278c409a4ea24dde1cc09664c6bfb5f
Lumaura <139267105+Lumaura@users.noreply.github.com>
2025-12-06 10:48:47 -0500

    Finished dialogue

--- Assets/Dialogue/Scripts/Dialogue.cs
+++ Assets/Dialogue/Scripts/Dialogue.cs
@@ -1,18 +1,19 @@
+using JetBrains.Annotations;
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System.IO;
 using System.Linq;
-using Unity.VisualScripting;
 using TMPro;
-using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
     TextAsset DialogueIntro;
     string dialogue;
     List<string> dialogueLines = new List<string>();
-    List<DialogueOption> dialogueOptions = new List<DialogueOption>();
+    public List<DialogueOption> dialogueOptions = new List<DialogueOption>();
     private int currentIndex = 0; //Conversation number
     private int choiceNum = 0;
     private bool choiceState = false;
@@ -26,106 +27,109 @@ public class Dialogue : MonoBehaviour
         DialogueIntro = Resources.Load<TextAsset>("Dialogues/DialogueIntro");
         dialogue = DialogueIntro.text;
         dialogueLines = dialogue.Split(new[] { '\n' }).ToList();
-        foreach (string line in dialogueLines)
+        foreach (string line in dialogueLines) //Loads and trims all dialogue types
         {
             dialogueOptions.Add(new DialogueOption());
-            if (line.Contains("~"))
+            if (line.Contains("+"))
             {
-                dialogueOptions[currentIndex].Text1.Add(line.Replace("~", "").Trim());
+                dialogueOptions[currentIndex].TextPlus.Add(line.Replace("+", "").Trim()); //Normal text
             }
-            else if (line.Contains("+"))
+            else if (line.Contains("~"))
             {
-                dialogueOptions[currentIndex].Text2.Add(line.Replace("+", "").Trim());
+                dialogueOptions[currentIndex].TextTilde.Add(line.Replace("~", "").Trim()); //Option prompts
             }
             else if (line.Contains("|"))
             {
-                dialogueOptions[currentIndex].optionText.Add(line.Replace("|", "").Trim());
+                dialogueOptions[currentIndex].optionText.Add(line.Replace("|", "").Trim()); //Options
             }
 
             else if (line.Contains("/"))
             {
-                dialogueOptions[currentIndex].responseLines.Add(line.Replace("/", "").Trim());
+                dialogueOptions[currentIndex].responseLines.Add(line.Replace("/", "").Trim()); //Responses
 
             }
-            else if (line.Contains("("))
+            else if (line.Contains("(")) //End index
             {
                 currentIndex++;
             }
         }
 
-        currentIndex = 0;
+        currentIndex = 0; //Initialize index
 
         foreach (string option in dialogueOptions[currentIndex].optionText)
         {
             Debug.Log(option);
         }
 
-        UISetup();
+        UISetup(); //Initialize UI and buttons
         
     }
 
     //STATES: BUTTON + PROMPT TEXT (choiceState = true), NORMAL TEXT (choiceState = false), RESPONSE TEXT (choiceState = true)
-    //HAVE TO SET CHOICE STATE TO FALSE AFTER RESPONSE IS GIVEN
-    //NEED TO FORWARD DIALOGUE AFTER RESPONSE IS GIVEN
 
     public void Update()
     {
-    //Press E, if buttons aren't active, goes to next dialogue set, then assumes that we are in normal text (which we are) and then makes sure buttons are inactive
+    //Press E, if you're in a response text, go to next index
+    //Otherwise, if you're in normal text, go to option choices (needs to be fixed, we need the ability to do consecutive normal text)
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            if (!buttonsActive)
+            if(!buttonsActive && choiceState)
             {
                 currentIndex++;
+                buttonsActive = false;
+                choiceState = false;
             }
-            else
+            else if(!buttonsActive && !choiceState)
             {
+                buttonsActive = true;
+                choiceState = true;
             }
-            choiceState = false;
-            buttonsActive = false;
+
+            UISetup();
             Debug.Log(choiceState);
             Debug.Log(buttonsActive);
 
         }
     }
-    public void buttonClicked(int optionNum)
+    public void buttonClicked(int optionNum) //When a button is clicked, set the response text
     {
         Buttons.SetActive(false);
         buttonsActive = false;
         choiceNum = optionNum;
         dialogueText.text = dialogueOptions[currentIndex].responseLines[optionNum];
-
     }
     public void UISetup()
     {
-        if (dialogueOptions[currentIndex].optionText.Count >= 1)
+        Buttons.SetActive(buttonsActive);
+        if (dialogueOptions[currentIndex].optionText.Count >= 1) //Set buttons to active or inactive, go through options and set choice options
         {
             for (int i = 0; i < dialogueOptions[currentIndex].optionText.Count; i++)
             {
                 choiceTexts[i].text = dialogueOptions[currentIndex].optionText[i];
             }
-            if (Buttons.gameObject.activeSelf == false && choiceState == false)
+
+
+            if (buttonsActive == false && choiceState == false) //If in a normal text state, load normal text
             {
-                dialogueText.text = dialogueOptions[currentIndex].Text2[0];
+                dialogueText.text = dialogueOptions[currentIndex].TextPlus[0];
             }
-            else if (Buttons.gameObject.activeSelf == true)
+            else if (buttonsActive && choiceState) // If in a button state, load option prompts
             { 
-                dialogueText.text = dialogueOptions[currentIndex].Text1[0];
-                buttonsActive = true;
+                dialogueText.text = dialogueOptions[currentIndex].TextTilde[0];
             }
         }
     }
 }
 
+[Serializable]
 public class DialogueOption
 {
-    public List<string> Text1 = new List<string>();
-    public List<string> Text2 = new List<string>();
+    public List<string> TextTilde = new List<string>();
+    public List<string> TextPlus = new List<string>();
     public List<string> optionText = new List<string>();
     public List<string> responseLines = new List<string>();
 
 }
-
-//interaction, set option num, search for response not option, debug
 
 
