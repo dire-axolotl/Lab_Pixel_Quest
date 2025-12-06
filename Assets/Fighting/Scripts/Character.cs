fa8bdd190194e12f9bfe838555f7e8c7fcbe1073
Lumaura <139267105+Lumaura@users.noreply.github.com>
2025-12-06 14:28:42 -0500

    Boss moves

--- Assets/Fighting/Scripts/Character.cs
+++ Assets/Fighting/Scripts/Character.cs
@@ -36,10 +36,10 @@ public class Character : MonoBehaviour
 
     public void shootFunc(int i, KeyCode k)
     {
-        print(getMove(i).getShoot() + " false");
+        //print(getMove(i).getShoot() + " false");
         if (Input.GetKeyDown(k) && getMove(i).getShoot())
         {
-            print("ran");
+            //print("ran");
             GameObject bullet1 = Instantiate(getMove(i).gameObject, bulletSpawn.position, Quaternion.identity);
             bullet1.SetActive(true);
             bullet1.transform.SetParent(bulletTrash);
@@ -61,16 +61,39 @@ public class Character : MonoBehaviour
         }
     }
 
+    public void fireShoot()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            for (int i = 0; i < 250; i++)
+            {
+                GameObject bullet1 = Instantiate(getMove(0).gameObject, bulletSpawn.position, Quaternion.identity);
+                bullet1.SetActive(true);
+                bullet1.transform.position += new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), 0);
+                bullet1.GetComponent<Rigidbody2D>().velocity = bullet1.transform.forward * 3000;
+            }
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
 
         //print(getMove(0).getShoot());
         for (int i = 0; i < _moveList.Length; i++) {
+            if (i == 1)
+            {
+                fireShoot();
+            }
+            else
+            {
+
+                fireShoot();
+            }
             canShootFunc(i);
-            shootFunc(i, getMove(i)._key);
             //print("hi");
-         }
+        }
         //if(Input.GetKeyDown(kryvofr));
     }
 }
